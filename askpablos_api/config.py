63f09a55c923affda92e13584d829977d690d003
Fawad <fawadstar6@gmial.com>
2026-03-26 16:13:15 +0500

    Update `DEFAULT_API_URL` to new endpoint in configuration.

--- askpablos_api/config.py
+++ askpablos_api/config.py
@@ -5,4 +5,4 @@ Configuration settings for the AskPablos API client.
 """
 
 # Default API configuration
-DEFAULT_API_URL = "http://10.10.10.178:7500/api/proxy/"
+DEFAULT_API_URL = "http://10.10.10.24"
